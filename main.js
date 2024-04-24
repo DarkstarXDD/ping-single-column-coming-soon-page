9af5b09284fb8327a81473f7941c7be51b34eda1
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2024-04-24 15:50:56 +0530

    add js form validation & aria attributes

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 import "./styles.css"
+import { z } from "zod"
+
+console.log("Hello, World!")
+let errorMessage = ""
+
+const formEl = document.querySelector(".form")
+const emailInputEl = document.querySelector(".input-email")
+const errorTextEl = document.querySelector(".error")
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Please provide a valid email address" }),
+})
+
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault()
+
+  const formData = {
+    email: emailInputEl.value,
+  }
+
+  const validationResult = formSchema.safeParse(formData)
+
+  if (!validationResult.success) {
+    const validationResultFormatted = validationResult.error.format()
+    errorMessage = validationResultFormatted.email?._errors.join(", ")
+    emailInputEl.focus()
+    emailInputEl.setAttribute("aria-invalid", true)
+    emailInputEl.classList.add("input-email-error")
+  } else {
+    errorMessage = ""
+    emailInputEl.value = ""
+    emailInputEl.blur()
+    emailInputEl.setAttribute("aria-invalid", false)
+    emailInputEl.classList.remove("input-email-error")
+  }
+  errorTextEl.innerHTML = `<p><i>${errorMessage}</i></p>`
+})
